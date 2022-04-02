42b1562bf11b248dd31f2f4455944290d4a71c8b
gitxuzan <gitxuzan@126.com>
2022-04-02 17:10:42 +0800

    v9

--- zap.go
+++ zap.go
@@ -2,60 +2,60 @@ package zlog
 
 
 func Info(args ...interface{}) {
-	F("sign","").Info( args...)
+	F("zlog","").Info( args...)
 }
 
 
 func  Error(args ...interface{}) {
-	F("sign","").Error(args...)
+	F("zlog","").Error(args...)
 }
 
 
 // Debug uses fmt.Sprint to construct and log a message.
 func  Debug(args ...interface{}) {
-	F("sign","").Debug(args...)
+	F("zlog","").Debug(args...)
 }
 
 
 // Warn uses fmt.Sprint to construct and log a message.
 func  Warn(args ...interface{}) {
-	F("sign","").Debug(args...)
+	F("zlog","").Debug(args...)
 }
 
 
 // Panic uses fmt.Sprint to construct and log a message, then panics.
 func Panic(args ...interface{}) {
-	F("sign","").Panic(args...)
+	F("zlog","").Panic(args...)
 }
 
 // Fatal uses fmt.Sprint to construct and log a message, then calls os.Exit.
 func Fatal(args ...interface{}) {
-	F("sign","").Fatal(args...)
+	F("zlog","").Fatal(args...)
 }
 
 func Infof(template string, args ...interface{}) {
-	F("sign","").Infof(template , args...)
+	F("zlog","").Infof(template , args...)
 }
 
 // Warnf uses fmt.Sprintf to log a templated message.
 func  Warnf(template string, args ...interface{}) {
-	F("sign","").Warnf(template , args...)
+	F("zlog","").Warnf(template , args...)
 }
 
 // Errorf uses fmt.Sprintf to log a templated message.
 func Errorf(template string, args ...interface{}) {
-	F("sign","").Errorf(template , args...)
+	F("zlog","").Errorf(template , args...)
 
 }
 
 
 // Panicf uses fmt.Sprintf to log a templated message, then panics.
 func  Panicf(template string, args ...interface{}) {
-	F("sign","").Panicf(template , args...)
+	F("zlog","").Panicf(template , args...)
 
 }
 
 // Fatalf uses fmt.Sprintf to log a templated message, then calls os.Exit.
 func  Fatalf(template string, args ...interface{}) {
-	F("sign","").Fatalf(template , args...)
+	F("zlog","").Fatalf(template , args...)
 }
